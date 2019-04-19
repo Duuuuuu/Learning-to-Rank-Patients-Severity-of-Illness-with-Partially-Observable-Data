4f24b8f77985596b450fa778ecd8ac1826f0d069
Duuuuuu <djzdevin@foxmail.com>
2019-04-19 14:32:34 -0400

    add mini-batch

--- dops.py
+++ dops.py
@@ -1,8 +1,14 @@
-# from autograd import numpy as np
-# from autograd import grad
-from cvxopt import matrix
-from cvxopt.solvers import qp
 import numpy as np
+
+
+def batch_idx(i, size, N):
+	if size >= N:
+		return np.arange(N)
+	idx1 = (i*size) % N
+	idx2 = ((i+1)*size) % N
+	if idx2 < idx1:
+		idx1 -= N
+	return np.arange(idx1, idx2)
 
 
 def shuffle_data(X, Y):
@@ -32,7 +38,8 @@ def phi_theta(theta, Si, C, alphaz):
 			ft, v = f_theta(theta, Si[y], C)
 			res.append(ft)
 			gradient += v
-	return sum(res)/alphaz.sum(), gradient/alphaz.sum()
+	sum_alphaz = alphaz.sum()
+	return sum(res)/sum_alphaz, gradient/sum_alphaz
 
 
 def obj_gradient(theta, Ss, zs, C, alphazs, lam=1, mu=1):
@@ -55,17 +62,19 @@ def obj_gradient(theta, Ss, zs, C, alphazs, lam=1, mu=1):
 	return theta + lam * gradient/N - mu * np.ones_like(theta)
 
 
-def sub_gradient_descent(theta, Ss, zs, C, alphazs, maxzs, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
+def sto_gradient_descent(theta, Ss, zs, C, alphazs, maxzs, batch_size=10, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
 	best_score = 0
 	best_theta = None
+	N = len(Ss)
 	for i in range(iters):
-		score = 0
-		gradient = obj_gradient(theta, Ss, zs, C, alphazs, lam, mu)
+		idx = batch_idx(i, batch_size, N)
+		gradient = obj_gradient(theta, Ss[idx], zs[idx], C, alphazs, lam, mu)
 		theta -= eta * gradient
 		if print_every > 0 and (i+1) % print_every == 0:
 			print('Iter %d, theta: %s, gradient: %s' % (i+1, theta, gradient))
 
 		# select the best one, but do we need this?
+		# score = 0
 		# for S, maxz in zip(Ss, maxzs):
 		# 	if max([f_theta(theta, s, C)[0] for s in S]) == maxz:
 		# 		score += 1
@@ -76,18 +85,18 @@ def sub_gradient_descent(theta, Ss, zs, C, alphazs, maxzs, eta=0.01, iters=1000,
 	return theta
 
 
-def dops(X, Y, T, C, m, alpha, init, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
+def dops(X, Y, T, C, m, alpha, init, batch_size=10, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
 	# config
 	M, d = X.shape
 	N = int(np.floor(M/m))
 	X, Y = shuffle_data(X, Y)
-	Ss = [X[i*m:(i+1)*m] for i in range(N)]
-	zs = [Y[i*m:(i+1)*m] for i in range(N)]
+	Ss = np.array([X[i*m:(i+1)*m] for i in range(N)])
+	zs = np.array([Y[i*m:(i+1)*m] for i in range(N)])
 	maxzs = [e.max() for e in zs]
 	alphazs = [np.array([1 if e >= maxz*alpha else 0 for e in z]) for z, maxz in zip(zs, maxzs)]
 
 	# optimize
-	theta = sub_gradient_descent(init, Ss, zs, C, alphazs, maxzs, eta, iters, lam, mu, print_every)
+	theta = sto_gradient_descent(init, Ss, zs, C, alphazs, maxzs, batch_size, eta, iters, lam, mu, print_every)
 	if theta is None:
 		raise ValueError('theta is None!')
 
--- dops_autograd.py
+++ dops_autograd.py
@@ -1,7 +1,15 @@
 from autograd import numpy as np
 from autograd import grad
-from cvxopt import matrix
-from cvxopt.solvers import qp
+
+
+def batch_idx(i, size, N):
+	if size >= N:
+		return np.arange(N)
+	idx1 = (i*size) % N
+	idx2 = ((i+1)*size) % N
+	if idx2 < idx1:
+		idx1 -= N
+	return np.arange(idx1, idx2)
 
 
 def shuffle_data(X, Y):
@@ -41,27 +49,29 @@ def objective(theta, Ss, zs, C, alphazs, lam=1, mu=1):
 	return 0.5 * np.dot(theta, theta) + lam * res/N - mu * theta.sum()
 
 
-def gradient_descent(theta, Ss, zs, C, alphazs, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
+def gradient_descent(theta, Ss, zs, C, alphazs, batch_size=10, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
 	gradident_fun = grad(objective)
+	N = len(Ss)
 	for i in range(iters):
-		theta -= eta * gradident_fun(theta, Ss, zs, C, alphazs, lam, mu)
+		idx = batch_idx(i, batch_size, N)
+		theta -= eta * gradident_fun(theta, Ss[idx], zs[idx], C, alphazs, lam, mu)
 		if print_every > 0 and (i+1) % print_every == 0:
 			print('Iter %d : %s' % (i+1, theta))
 	return theta
 
 
-def dops(X, Y, T, C, m, alpha, init, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
+def dops(X, Y, T, C, m, alpha, init, batch_size=10, eta=0.01, iters=1000, lam=1, mu=1, print_every=10):
 	# config
 	M, d = X.shape
 	N = int(np.floor(M/m))
 	X, Y = shuffle_data(X, Y)
-	Ss = [X[i*m:(i+1)*m] for i in range(N)]
-	zs = [Y[i*m:(i+1)*m] for i in range(N)]
+	Ss = np.array([X[i*m:(i+1)*m] for i in range(N)])
+	zs = np.array([Y[i*m:(i+1)*m] for i in range(N)])
 	maxzs = [e.max() for e in zs]
 	alphazs = [np.array([1 if e >= maxz*alpha else 0 for e in z]) for z, maxz in zip(zs, maxzs)]
 
 	# optimize
-	theta = gradient_descent(init, Ss, zs, C, alphazs, eta, iters, lam, mu, print_every)
+	theta = gradient_descent(init, Ss, zs, C, alphazs, batch_size, eta, iters, lam, mu, print_every)
 
 	# get result
 	res = [f_theta(theta, t, C) for t in T]
